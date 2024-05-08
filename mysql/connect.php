b02e6e712b211cc5a1c29de72720538873cf2236
Sherif <127263385+Sherif-lotfy@users.noreply.github.com>
2024-05-08 08:15:19 +0300

    Update connect.php

--- mysql/connect.php
+++ mysql/connect.php
@@ -8,4 +8,7 @@
     $connectdb = mysqli_connect($host , $user , $password , $dbname);
     // if($connectdb){
     //     echo "connected <br>";
+    
+
     // }
+-new pdo("mysql:host=$host,$dbname",,)
